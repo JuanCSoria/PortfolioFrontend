a9918f0d90c33674bf2c48304d3b7feea1201c76
JuanCSoria <usuariojcs@gmail.com>
2023-01-16 00:37:14 -0300

    Frontend con Agular y BootStrap 5.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'portfolio', component:PortfolioComponent},
+	{path:'iniciar-sesion', component: IniciarSesionComponent},
+  {path:'',redirectTo:'iniciar-sesion', pathMatch:'full'},
+  {path:'contacto', component:ContactoComponent}
+	
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { APlogoComponent } from './componentes/aplogo/aplogo.component';
+import { BotoneraComponent } from './componentes/botonera/botonera.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { NgCircleProgressModule} from 'ng-circle-progress';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    APlogoComponent,
+   BotoneraComponent,
+   BannerComponent,
+   AcercadeComponent,
+   ExperienciaComponent,
+   EducacionComponent,
+   HabilidadesComponent,
+   ProyectosComponent,
+   FooterComponent,
+   ToolbarComponent,
+   PortfolioComponent,
+   IniciarSesionComponent,
+   ContactoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
